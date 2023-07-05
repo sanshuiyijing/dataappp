7ae6d23c30cc10d306b67e40055796becebf41cf
sanshuiyijing <jingdeveloper@126.com>
2023-07-05 11:24:48 +0800

    dataservice refresh save

--- http_endpoints/sql/GET-dsfd.sql
+++ http_endpoints/sql/GET-dsfd.sql
@@ -3,6 +3,6 @@ Enter "USE {database};" before entering your SQL statements.
 Type "--your question" + Enter to try out AI-generated SQL queries
 Declare a parameter like "Where id = ${arg}".
 */
-SELECT "Hello World111";
+SELECT "Hello World111222";
 
 
